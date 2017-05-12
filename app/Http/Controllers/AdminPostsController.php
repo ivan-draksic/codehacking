170aca95ac37f1b2c736bfd7e8840725a547a0fd
Ivan <ivan.draks21@gmail.com>
2017-05-12 23:31:13 +0200

    able to create posts with dynamic categories

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Post;
 use App\Http\Requests\PostsCreateRequest;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use App\Photo;
+use App\Category;
+
 
 class AdminPostsController extends Controller
 {
@@ -31,8 +33,9 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::lists('name', 'id')->all();
 
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
